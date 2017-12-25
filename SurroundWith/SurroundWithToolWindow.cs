70eb00aeaf315fba96ed60f183d4f6c72ed5c385
Ali Zahid <ali.zad@live.com>
2017-12-25 15:52:33 +0800

    boilerplate enable-disable command and menu command added

---
+++ SurroundWith/SurroundWithToolWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace SurroundWith
+{
+    using Microsoft.VisualStudio.Shell;
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("8e4b8a21-4f76-46cd-a3bf-04a2c07adc38")]
+    public class SurroundWithToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurroundWithToolWindow"/> class.
+        /// </summary>
+        public SurroundWithToolWindow() : base(null)
+        {
+            this.Caption = "SurroundWithToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new SurroundWithToolWindowControl();
+        }
+    }
+}
---
+++ SurroundWith/SurroundWithToolWindowPackage.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace SurroundWith
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(SurroundWithToolWindow))]
+    [Guid(SurroundWithToolWindowPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class SurroundWithToolWindowPackage : Package
+    {
+        /// <summary>
+        /// SurroundWithToolWindowPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "7bf7d551-8d14-4714-8d38-411f00f188c3";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurroundWithToolWindow"/> class.
+        /// </summary>
+        public SurroundWithToolWindowPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            SurroundWithToolWindowCommand.Initialize(this);
+            base.Initialize();
+            EnableDisableCommand.Initialize(this);
+        }
+
+        #endregion Package Members
+    }
+}
