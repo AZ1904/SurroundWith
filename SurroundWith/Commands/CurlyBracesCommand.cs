39cb9a6993cb9707e4898a598cbfc924a3499fa0
Ali Zahid <ali.zad@live.com>
2018-01-01 19:15:51 +0800

    get the code in CurlyBracesCommand and call ToggleCurlyBraces on the _toggle object

--- SurroundWith/Commands/CurlyBracesCommand.cs
+++ SurroundWith/Commands/CurlyBracesCommand.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
 using System;
 using System.ComponentModel.Design;
-using System.Globalization;
 
 namespace SurroundWith.Commands
 {
@@ -24,17 +24,21 @@ namespace SurroundWith.Commands
         /// </summary>
         private readonly Package _package;
 
+        private readonly ToggleCommands _toggle;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="CurlyBracesCommand"/> class.
         /// Adds our command handlers for menu (commands must exist in the command table file)
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        private CurlyBracesCommand(Package package)
+        private CurlyBracesCommand(Package package, ToggleCommands toggleCommands)
         {
             _package = package ?? throw new ArgumentNullException(nameof(package));
 
             if (!(ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService))
                 return;
+
+            _toggle = toggleCommands;
 
             var menuCommandId = new CommandID(CommandSet, CommandId);
             var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
@@ -59,7 +63,7 @@ namespace SurroundWith.Commands
         /// Initializes the singleton instance of the command.
         /// </summary>
         /// <param name="package">Owner package, not null.</param>
-        public static void Initialize(Package package) => Instance = new CurlyBracesCommand(package);
+        public static void Initialize(Package package, ToggleCommands toggleCommands) => Instance = new CurlyBracesCommand(package, toggleCommands);
 
         /// <summary>
         /// This function is the callback used to execute the command when the menu item is clicked.
@@ -70,6 +74,19 @@ namespace SurroundWith.Commands
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
+            var dte = (DTE2)ServiceProvider.GetService(typeof(DTE));
+
+            if (dte.ActiveDocument == null) return;
+
+            var selection = (TextSelection)dte.ActiveDocument.Selection;
+            string text = selection.Text;
+
+            if (!string.IsNullOrEmpty(text))
+                selection.Text = _toggle.ToggleCurlyBraces(text);
+            else
+            {
+                //todo: show something in the status-bar
+            }
         }
     }
 }
